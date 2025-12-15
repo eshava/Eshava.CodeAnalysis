1adda587f73a731a9fb4322ca93a12761607a062
TinoKrueger <krueger.tino@googlemail.com>
2025-12-15 17:18:00 +0100

    Added extension method for nesting if-else statements

--- Eshava.CodeAnalysis/Extensions/IfStatementSyntaxExtensions.cs
+++ Eshava.CodeAnalysis/Extensions/IfStatementSyntaxExtensions.cs
@@ -13,5 +13,21 @@ namespace Eshava.CodeAnalysis.Extensions
 		{
 			return SyntaxHelper.CreateElseIfStatement(ifStatement, SyntaxHelper.CreateIfStatement(elseIfStatement, statements));
 		}
+
+		public static IfStatementSyntax ElseIf(this IfStatementSyntax ifStatement, IfStatementSyntax[] elseIfStatements, params StatementSyntax[] elseStatements)
+		{
+			var local = elseIfStatements[elseIfStatements.Length - 1];
+			if (elseStatements.Length > 0)
+			{
+				local = SyntaxHelper.CreateElseStatement(elseIfStatements[elseIfStatements.Length - 1], elseStatements);
+			}
+
+			for (var i = elseIfStatements.Length - 2; i >= 0; i--)
+			{
+				local = SyntaxHelper.CreateElseIfStatement(elseIfStatements[i], local);
+			}
+
+			return SyntaxHelper.CreateElseIfStatement(ifStatement, local);
+		}
 	}
 }
